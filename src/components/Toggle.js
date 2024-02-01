9a97a1a86dde6be50aecb26e5aa1fce94a09eb2b
Priscilla-22 <priscillamw123@gmail.com>
2024-02-01 17:57:28 +0300

    fix: Correct toggle button functionality

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  const handleClick=()=>setIsOn(!isOn)
+  return (
+    <button onClick={handleClick}>
+      {isOn ? 'ON' : 'OFF'}
+    </button>
+  );
 }
 
 export default Toggle;
